7ebf8975c5fa3c1d3d8d835bf499f515b545f902
trancamvinh2001 <69330923+trancamvinh2001@users.noreply.github.com>
2020-08-11 17:01:54 +0700

    Update Exercise4.c

--- Exercise4.c
+++ Exercise4.c
@@ -15,7 +15,29 @@ Ex:
 #define SIZE 1000
 
 void ex4(char s1[], char s2[]){
-	//your codes here
+	int n,i,j,k,a[10];
+	int sum=0;
+	i=strlen(s1)-1;
+	j=strlen(s2)-1;
+	while(i>=0||j>=0||sum!=0)
+	{
+		if(i>=0)
+		{
+			sum=sum+(s1[i]-48);
+		}
+		if(j>=0)
+		{
+			sum=sum+(s2[j]-48);
+		}
+		n=sum%2;
+		a[k]=n;
+		sum=sum/2;
+		i--;j--;
+		k++;
+	}
+	for(i=k-1;i>=0;i--){
+		printf("%d",a[i]);
+	}
 }
 
 int main(int argc, char *argv[]) {
