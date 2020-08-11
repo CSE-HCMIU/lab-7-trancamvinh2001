d2b09cc48c8a99f623f966238a4d73a703ad389a
trancamvinh2001 <69330923+trancamvinh2001@users.noreply.github.com>
2020-08-11 16:53:42 +0700

    Update Exercise2.c

--- Exercise2.c
+++ Exercise2.c
@@ -16,7 +16,29 @@ Ex:
 #define SIZE 1000
 
 void ex2(char *str[],int n){
-	//your codes here
+	int i,j,count;
+    	int newlen;
+    	for(i=0;i<n;i++)
+    	{
+    		for(j=i+1;j<n;j++)
+    		{
+            		int current=strcmp(str[i],str[j]);
+            		if(current==0)
+            		{
+            			str[j]="\0";
+			}
+		}
+ 	}
+	for(i=0;i<n;i++)
+    	{
+     		if(str[i]=="\0")
+     		{
+		  count++;
+	    	}
+     	
+    	}
+	newlen=n-count;
+	printf("%d",newlen);
 }
 
 int main(int argc, char *argv[]) {
